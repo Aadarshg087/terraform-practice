8049dca79beb59fc70d1ac6e329019a4a429cf77
aadarsh <aadarshg087@gmail.com>
2025-12-22 20:31:17 +0530

    Added: Launch script to the ec2 instance

--- application/main.tf
+++ application/main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "ec2-application" {
   ami = "ami-0ecb62995f68bb549"
 
   instance_type = "t3.micro"
+
+  user_data = <<-EOF
+  #!/bin/bash
+  sudo apt update -y
+  echo "hello" >> ~/temp.txt
+  EOF
+
+  vpc_security_group_ids = [aws_security_group.allow_comm.id]
+
   tags = {
     Purpose     = "Learning"
     Environment = "dev"
@@ -31,6 +40,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_incoming" {
   from_port   = 8000
   ip_protocol = "tcp"
   to_port     = 8000
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh_connect" {
+  security_group_id = aws_security_group.allow_comm.id
+  cidr_ipv4         = "0.0.0.0/0"
+  #   cidr_ipv4         = data.aws_vpc.default.cidr_block
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
 }
 
 
